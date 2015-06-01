94e2e5bc7e54466a22afc7797a60759b70ad6095
toady <support@8pecxstudios.com>
2015-06-01 11:47:28 +1000

    Added: Restart now button to restart notification doorhanger.
    Users can restart before the pre-set time.

--- js/cancel.js
+++ js/cancel.js
@@ -1,28 +1,28 @@
 	/*
 
-				The MIT License (MIT)
+					The MIT License (MIT)
 
-				Copyright (c) 2015 8pecxstudios.com 
+					Copyright (c) 2015 8pecxstudios.com 
 
-				Permission is hereby granted, free of charge, to any person obtaining a copy
-				of this software and associated documentation files (the "Software"), to deal
-				in the Software without restriction, including without limitation the rights
-				to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-				copies of the Software, and to permit persons to whom the Software is
-				furnished to do so, subject to the following conditions:
+					Permission is hereby granted, free of charge, to any person obtaining a copy
+					of this software and associated documentation files (the "Software"), to deal
+					in the Software without restriction, including without limitation the rights
+					to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+					copies of the Software, and to permit persons to whom the Software is
+					furnished to do so, subject to the following conditions:
 
-				The above copyright notice and this permission notice shall be included in
-				all copies or substantial portions of the Software.
+					The above copyright notice and this permission notice shall be included in
+					all copies or substantial portions of the Software.
 
-				THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-				IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-				FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-				AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-				LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-				OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-				THE SOFTWARE.
+					THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+					IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+					FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+					AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+					LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+					OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+					THE SOFTWARE.
 
-			*/
+				*/
 	var restartmychromecancel = {
 	    init: function() {
 	        try {
@@ -33,6 +33,7 @@
 	            );
 	            document.getElementById('cancelTitle').textContent = chrome.i18n.getMessage("appCancelTitle");
 	            document.getElementById('cancelRestartIn').textContent = chrome.i18n.getMessage("appCancelRestartIn");
+	            document.getElementById('restartNowButton').textContent = chrome.i18n.getMessage("appCancelRestartNowButton");
 	            document.getElementById('cancelButton').textContent = chrome.i18n.getMessage("appCancelButton");
 	        } catch (e) {
 	            alert("An error was encountered while initializing cancel.js " + e);
@@ -41,6 +42,13 @@
 	};
 
 	document.addEventListener('DOMContentLoaded', restartmychromecancel.init);
+	document.getElementById('restartNow').addEventListener('click', function() {
+	    try {
+	        chrome.extension.getBackgroundPage().restartmychrome.browserRestart();
+	    } catch (e) {
+	        alert("An error was encountered while triggering the restart now button click event " + e);
+	    }
+	});
 	document.getElementById('cancel').addEventListener('click', function() {
 	    try {
 	        chrome.extension.getBackgroundPage().restartmychrometimer.timedRestart("", false, 0);
